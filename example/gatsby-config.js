3ac4c3e005e0d52df6fc2550499e17aa6c46cc9f
Mogeko <zhengjunyi@live.com>
2021-10-15 18:36:29 +0000

    docs: add comments to gatsby-config.js

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,12 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Terminal',
-    themeColor: '#8c3a00',
-    year: 2017,
+    title: 'Terminal', // The name of the site
+    themeColor: '#8c3a00', // The theme color of the website, default '#8c3a00'
+    year: 2017, // When did you start writing articles? (year)
     menu: [
+      // The structure of the main menu
+      // Pay attention to the order
       {
-        title: 'About',
-        path: '/about',
+        title: 'About', // Name of the menu
+        path: '/about', // URL of the menu
       },
       {
         title: 'Tags',
